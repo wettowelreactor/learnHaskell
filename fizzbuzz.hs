8ef942698aec6ce3471b298da2264f518846008b
Camille Roussel <camille@rousselfamily.com>
2015-04-09 22:29:24 -0700

    show x

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,1 +1,1 @@
-fizzBuzzItem item = [if x `mod` 15 == 0 then "FIZZBUZZ" else if x `mod` 3 == 0 then "Fizz" else if x `mod` 5 == 0 then "Buzz" else "x" | x <- item ]
+fizzBuzzItem item = [if x `mod` 15 == 0 then "FIZZBUZZ" else if x `mod` 3 == 0 then "Fizz" else if x `mod` 5 == 0 then "Buzz" else show x | x <- item ]
